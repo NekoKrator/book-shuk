0b9a52636ff753896bc077e69acf7aa3111e181b
Fil Daniil <irarbc89@gmail.com>
2024-10-12 00:49:47 +0300

    updated auth microservice

--- microservices/auth-service/auth.js
+++ microservices/auth-service/auth.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const { User } = require('../shared/models/user-model')
+const { connectDB } = require('./src/database/auth-db')
+const { errorHandler } = require('../shared/middlewares/error-handler')
 require('dotenv').config
 
 const app = express()
@@ -9,25 +11,35 @@ const PORT = process.env.PORT || 3001
 
 app.use(express.json())
 
-// reg
-app.post('/register', async (req, res) => {
-    const { email, username, password } = req.body
-    const hashedPassword = await bcrypt.hash(password, 10)
-    const newUser = new User({ email, username, password: hashedPassword })
-    await newUser.save()
-    res.status(201).json({ message: 'User registered successfully' })
+connectDB()
+
+app.post('/register', async (req, res, next) => {
+    try {
+        const { email, username, password } = req.body
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const newUser = new User({ email, username, password: hashedPassword })
+        await newUser.save()
+        res.status(201).json({ message: 'User registered successfully' })
+    } catch (error) {
+        next(error)
+    }
 })
 
-// log
 app.post('/login', async (req, res) => {
-    const { email, password } = req.body
-    const user = await User.findOne({ email })
-    if (!user || (await bcrypt.compare(password, user.password))) {
-        return res.status(400).json({ message: 'Invalid email or password' })
+    try {
+        const { email, password } = req.body
+        const user = await User.findOne({ email })
+        if (!user || (await bcrypt.compare(password, user.password))) {
+            return res.status(400).json({ message: 'Invalid email or password' })
+        }
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY)
+        res.status(200).json({ token })
+    } catch (error) {
+        next(error)
     }
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY)
-    res.status(200).json({ token })
 })
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log(`Auth service running on port ${PORT}`)
